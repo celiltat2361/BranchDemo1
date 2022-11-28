7f5cbcf90f1ce9bad27e703e40972e3271514d4c
Celil Tat <celilbtc@gmail.com>
2022-11-28 23:06:06 +0100

    Add project files.

---
+++ BranchDemo1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BranchDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
